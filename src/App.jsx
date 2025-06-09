974055e258513fb1bdf29c94708e60aa5bb9243f
theBabaT <nico.tekdal@yahoo.de>
2025-06-10 00:00:08 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminPage from './pages/AdminPage';
+import DebugPage from './pages/debugpage';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/" element={<h2>Willkommen zum EKG-Quiz</h2>} />
+        <Route path="/debug" element={<debugpage />} />
       </Routes>
     </Router>
   );
