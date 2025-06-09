f8e98096f256ec32ba3b99b8c4fc318620799ee2
theBabaT <nico.tekdal@yahoo.de>
2025-06-10 00:02:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,7 @@ import App from './App';
 import { QuestionProvider } from './context/QuestionContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <QuestionProvider><App /></QuestionProvider>
+  <QuestionProvider>
+    <App />
+  </QuestionProvider>
 );
